43672c95929f04e4f07eb996102185b4066c296c
andela-ggikera <githinji.gikera@andela.com>
2015-12-13 00:14:18 +0300

    create RESTful test spec

---
+++ spec/spec.js
@@ -0,0 +1,66 @@
+// jasmine test for the Rest API
+var frisby = require('frisby');
+
+frisby.create('GET: Request all the names from collection')
+  .get('http://localhost:8080/api/simple')
+    .expectStatus(200)
+    .expectHeaderContains('content-type', 'application/json')
+    .expectJSON('0', {
+    	_id: function(val) { expect(val).toMatchOrBeNull("566c312ce7909fad0719e3e1") },
+    	__v: 0,
+    	name: function(val) { expect(val).toMatchOrBeNull("Becky Burner") },
+    })
+    .expectJSONTypes('0', {
+    	_id: String,
+	    name: function(val) { expect(val).toBeTypeOrNull(String); },
+	  })
+
+frisby.create('GET: Request a given name from collection')
+  .get('http://localhost:8080/api/simple/566c312ce7909fad0719e3e1')
+    .expectStatus(200)
+    .expectHeaderContains('content-type', 'application/json')
+    .expectJSON('0', {
+    	_id: function(val) { expect(val).toMatchOrBeNull("566c312ce7909fad0719e3e1") },
+    	__v: 0,
+    	name: "Becky Burner"
+    })
+    .expectJSONTypes('0', {
+    	_id: String,
+	    name: function(val) { expect(val).toBeTypeOrNull(String); },
+	  })
+
+frisby.create('POST: Add a new name to collection')
+	.post('http://localhost:8080/api/simple/Jee Githinji Gikera')
+		.expectStatus(200)
+		.expectHeaderContains('content-type', 'application/json')
+		.expectJSON({
+    	message: function(val) { expect(val).toMatchOrBeNull(String); },
+    })
+		.expectJSONTypes({
+	    message: function(val) { expect(val).toBeTypeOrNull(String); },
+	  })
+
+frisby.create('PUT: Edit a given name in the collection')
+	.put('http://localhost:8080/api/simple/566c312ce7909fad0719e3e1')
+		.expectStatus(200)
+		.expectHeaderContains('content-type', 'application/json')
+		.expectJSON({
+    	message: function(val) { expect(val).toMatchOrBeNull(String); },
+    })
+		.expectJSONTypes({
+	    message : function(val) { expect(val).toBeTypeOrNull(String); },
+	  })
+
+frisby.create('DELETE: a given name from the collection')
+	.delete('http://localhost:8080/api/simple/566c2df734c9b23207f63c94')
+		.expectStatus(200)
+		.expectHeaderContains('content-type', 'application/json')
+		.expectJSON({
+    	message: function(val) { expect(val).toMatchOrBeNull('Successfully deleted it') },
+    })
+		.expectJSONTypes({
+	    message : function(val) { expect(val).toBeTypeOrNull(String); }
+	  })
+.toss();
+
+
